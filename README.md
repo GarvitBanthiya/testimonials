bd59c5414a0fcff8f389d0e28f81dfb303d01332
GarvitBanthiya <banthiyagarvit@gmail.com>
2025-01-31 01:21:10 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+### Getting Started
+ Follow the steps below to set up and run the project locally.
+ #### 1. Clone the Repository
+ #### 2. Install Dependencies
+ * npm install
+ * npm i react-icons
+ * npm i react-toastify
+ #### 3. Start Tailwind CSS CLI
+ Open a separate terminal and run:
+ * npx @tailwindcss/cli -i ./src/index.css -o ./public/style.css --watch
+ #### 4. Start the Development Server
+ In another terminal, start the React development server:
+ * npm run start
---
+++ src/data.js
@@ -0,0 +1,45 @@
+import garvit from "./assets/garvit.png";
+import bhumit from "./assets/bhumit.png";
+import bhuvnesh from "./assets/bhuvnesh.png";
+import neha from "./assets/neha.png";
+import mudit from "./assets/mudit.png";
+
+const reviews = [
+  {
+    id: 1,
+    name: "Garvit Banthiya",
+    job: "Software Engineer",
+    image: garvit,
+    text: "I have had the pleasure of working with this team on several projects, and I am consistently impressed with their technical expertise and ability to deliver quality solutions on time and within budget. They are a true partner and an asset to any project.",
+  },
+  {
+    id: 2,
+    name: "Bhumit Jain",
+    job: "Graphic Designer",
+    image: bhumit,
+    text: "I have been working with this company for several years now, and I have always been impressed with their creativity and attention to detail. They are true professionals who take pride in their work and always go above and beyond to deliver exceptional results.",
+  },
+  {
+    id: 3,
+    name: "Bhuvnesh Saini",
+    job: "AI Expert",
+    image: bhuvnesh,
+    text: "I am thrilled with the results of our recent AI bot marketing campaign, and it wouldn't have been possible without the hard work and dedication of the entire team. Thank you for your exceptional work!",
+  },
+  {
+    id: 4,
+    name: "Neha Jain",
+    job: "Salesforce Expert",
+    image: neha,
+    text: "I had the opportunity to work with this team on a data analysis project, and I was impressed with their attention to detail and their ability to work with complex datasets. They provided valuable insights that helped us make informed business decisions. It was a pleasure working with them.",
+  },
+  {
+    id: 5,
+    name: "Mudit Khandelwal",
+    job: "Human Resources",
+    image: mudit,
+    text: "I have worked with many content creators over the years, but none have impressed me as much as this team. They have a knack for crafting compelling and engaging content that resonates with our audience and drives real results. I highly recommend them!",
+  },
+];
+
+export default reviews;
